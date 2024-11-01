12dcb58aad31eb793dd8482db6638dc302f07c0a
santos naidu <santhosh333nd@gmail.com>
2024-11-01 18:38:27 +0530

    nw push

---
+++ src/programming/Drone.java
@@ -0,0 +1,21 @@
+package programming;
+
+public class Drone {
+    
+    public static void main(String[] args) {
+   
+    	int i=10;
+    	if(i>100) {
+    		System.out.println("i greater than 100");
+    	} else if(i==88) {
+    		System.out.println("i equal to 88");
+    	} else if (i==76) {
+    		System.out.println("i eaual to 77");
+    	} else {
+    		System.out.println("print this");
+    	}
+    	
+    	
+    	
+    }
+}
---
+++ src/programming/PrintNames.java
@@ -0,0 +1,16 @@
+package programming;
+
+public class PrintNames {
+
+	public static void main(String[] args) {
+
+	String[] names= {
+			"Alice", "peter", "Done"
+	};
+	for (int i=0; i <names.length; i ++) {
+		System.out.println("Name:" + names[i]);
+	}
+		
+	}
+
+}
